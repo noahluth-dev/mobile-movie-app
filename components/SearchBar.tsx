35e5131bab3621584dcc277732436568c2dbc359
Sinna_Shan <luth595@gmail.com>
2025-12-30 12:26:34 +0530

    feat: update app icons and add SearchBar component for improved navigation

---
+++ components/SearchBar.tsx
@@ -0,0 +1,21 @@
+import { icons } from '@/constants/icons'
+import React from 'react'
+import { Text, View, Image, TextInput } from 'react-native'
+interface props { onPress: () => void; placeHolder: string }
+
+const SearchBar = ({ onPress, placeHolder }: props) => {
+  return (
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8dff" />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeHolder}
+        value='' 
+        onChangeText={() => { }}
+        placeholderTextColor='#a8b5db'
+        className='flex-1 ml-2 text-white' />
+    </View>
+  )
+}
+
+export default SearchBar
