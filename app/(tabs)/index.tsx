807aae822ae0297a20a830062bc6d4a524609fe2
Sinna_Shan <luth595@gmail.com>
2025-12-30 11:57:15 +0530

    Add icon and image constants, define movie interfaces, and declare PNG module

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center"
-    >
+    <View className="flex-1 items-center justify-center">
       <Text className="text-5xl text-pretty font-bold">Welcome!</Text>
     </View>
   );
