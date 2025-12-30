a2d63f9c24a126ac8fa13e0d24149e4439ea79cb
Sinna_Shan <luth595@gmail.com>
2025-12-30 14:28:53 +0530

    feat: implement movie fetching and display with MovieCard component, add loading and error handling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { Link, useRouter } from "expo-router";
-import { Text, View, Image, ScrollView } from "react-native";
+import { Text, View, Image, ScrollView, ActivityIndicator, FlatList } from "react-native";
 
 export default function Index() {
-  const router=useRouter();
+  const router = useRouter();
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: '' }));
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0 " />
@@ -17,12 +21,34 @@ export default function Index() {
           paddingBottom: 10,
         }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5">
-          <SearchBar 
-          onPress={()=>router.push('/search')}
-          placeHolder="Search for a movie"
-          />
-        </View>
+        {moviesLoading
+          ? (<ActivityIndicator size='large' color='0000ff' className="mt-10 self-center" />)
+          : moviesError
+            ? (<Text>Error: {moviesError?.message}</Text>)
+            : (<View className="flex-1 mt-5">
+              <SearchBar
+                onPress={() => router.push('/search')}
+                placeHolder="Search for a movie"
+              />
+              <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+              <FlatList data={movies} renderItem={({item})=>(
+                <MovieCard {...item}/>
+              )}
+              keyExtractor={(item)=>item.id.toString()}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent:'flex-start',
+                gap:20,
+                paddingRight:5,
+                marginBottom:10
+              }}
+              className="mt-2 pb-32"
+              scrollEnabled={false}
+              />
+              </>
+            </View>)}
+
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -13,7 +13,7 @@ export const TMDB_CONFIG = {
 
 export const fetchMovies = async ({ query }: { query: string }) => {
     const endPoint = query
-        ? `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=${encodeURIComponent(query)}`
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
         : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
     const response = await fetch(endPoint, {
